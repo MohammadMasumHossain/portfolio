d4bdb06aa3e6262d821b7c64e6dd9a02298e1fb3
mohammad Masum Hossain <masumhossainrana71@gmail.com>
2025-06-28 21:50:44 +0600

    setup

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import MainLayout from './Layouts/MainLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    
+     
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
